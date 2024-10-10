dd05d59f7b0c493e8908b7ddbed6c18f1f5b310c
Sandile891 <mnisisandile16@gmail.com>
2024-10-10 16:13:01 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/CSS Bus.css', // Add your CSS files here
-  '/JS bus.js',  // Add your JavaScript files here
+  'index.html',
+  'manifest.json',
+  'CSS Bus.css', // Add your CSS files here
+  'JS bus.js',  // Add your JavaScript files here
   // Add other assets like images, etc.
 ];
 
@@ -47,43 +47,6 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-/////
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-const CACHE = "pwabuilder-offline-page";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
