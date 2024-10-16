7f9ba5ba5d45fd9f9b2d94f818355655a5fdb4ad
Sandile891 <mnisisandile16@gmail.com>
2024-10-16 10:47:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -90,3 +90,11 @@ self.addEventListener('notificationclick', function(event) {
   );
 });
 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
